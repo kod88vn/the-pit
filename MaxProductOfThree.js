ae0cecc65bda0548d800ae4410a0b40d74bb89f3
Jay Quach <kod88vn@gmail.com>
2018-12-11 18:08:23 -0500

    Added MaxProductOfThree

--- MaxProductOfThree.js
+++ MaxProductOfThree.js
@@ -1,5 +1,5 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
+// Biggest numbers are at the 2 ends. However, the nagetives are only good if they provide a pair.
+// Special case is there are only negative numbers, in which the final result have to be negative, the we only consider the biggest
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
@@ -24,17 +24,18 @@ function solution(A) {
 
     let a = bottomThree[0];
     let b = bottomThree[1];
-    let c = bottomThree[2];
 
+    // if all are negative. Multiply the biggests
     if(topThree[2] < 0) {
         topThree[0]*topThree[1]*topThree[2];
     }
-
     
+    // consider a pair of biggest negatives
     if(a * b > 0) {
         max = a*b*topThree[2];
     }
     
+    // then consider only the biggest
     if(topThree[0]*topThree[1]*topThree[2] > max) {
         max = topThree[0]*topThree[1]*topThree[2];
     }
